82a371961976e3d86284902de290c3093ab0e4de
Artem Kazakov <opensource@akazakov.net>
2016-03-17 20:49:07 +0300

    Improved gulp build

--- static/src/gulpfile.js
+++ static/src/gulpfile.js
@@ -1,12 +1,8 @@
 var gulp = require('gulp');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var uglify = require('gulp-uglify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
 var less = require('gulp-less');
 var concat = require('gulp-concat');
 var gutil = require('gulp-util');
+var makeItFaster = require('spawn-task-experiment').spawn;
 
 
 var LessPluginCleanCSS = require('less-plugin-clean-css'),
@@ -15,35 +11,49 @@ var LessPluginCleanCSS = require('less-plugin-clean-css'),
     autoprefix = new LessPluginAutoPrefix({ browsers: ["last 5 versions"] });
 
 
-function buildJsx(entry_point, out_dir, out_file) {
-    browserify({
-        entries: entry_point,
-        extensions: ['.jsx'],
-        paths: ['./src/jsx/'],
-        debug: gutil.env.type !== 'production',
-    })
-    .transform(babelify, {
-        presets: ['es2015-loose', 'react'],
-        plugins: ['transform-class-properties'],
-    })
-    .bundle()
-    .pipe(source(out_file))
-    .pipe(buffer())
-    .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
-    .pipe(gulp.dest(out_dir));
-}
-
 var all_jsx_tasks = [];
 
 function createJsxTask(task) {
     all_jsx_tasks.push(task);
-    gulp.task(task, function() {
-        buildJsx(
+    function doTheJob() {
+        var gulp = require('gulp');
+        var browserify = require('browserify');
+        var babelify = require('babelify');
+        var uglify = require('gulp-uglify');
+        var source = require('vinyl-source-stream');
+        var buffer = require('vinyl-buffer');
+        var gutil = require('gulp-util');
+
+        var task = "__task__";
+        var env_type = "__env_type__";
+        function buildJsx(entry_point, out_dir, out_file) {
+            var bundler = browserify({
+                entries: entry_point,
+                extensions: ['.jsx'],
+                paths: ['./src/jsx/'],
+                debug: env_type !== 'production',
+            })
+            .transform(babelify, {
+                presets: ['es2015-loose', 'react'],
+                plugins: ['transform-class-properties'],
+            })
+            .bundle()
+            .pipe(source(out_file))
+            .pipe(buffer())
+            .pipe(env_type === 'production' ? uglify() : gutil.noop())
+            .pipe(gulp.dest(out_dir));
+            return bundler;
+        }
+        return buildJsx(
             'src/jsx/' + task + '.jsx',
             '../js',
             task + '.js'
         );
-    });
+    }
+    var str_func = doTheJob.toString()
+        .replace("__task__", task)
+        .replace("__env_type__", gutil.env.type);
+    gulp.task(task, makeItFaster(str_func));
 }
 
 createJsxTask('start_page');
@@ -57,12 +67,6 @@ createJsxTask('presenter');
 createJsxTask('screen');
 createJsxTask('screen_operator');
 
-gulp.task('all', all_jsx_tasks);
+gulp.task('all', gulp.parallel(...all_jsx_tasks));
 
-gulp.task('watch', function() {
-    gulp.watch('src/**', function() {
-        gulp.run('all');
-    });
-});
-
-gulp.task('default', ['all']);
+gulp.task('default', gulp.series('all'));
