ec6eefab9de41e86b2ab6a422f70298f3bf73310
Artem Kazakov <opensource@akazakov.net>
2016-11-10 01:34:51 +0300

    Bumped version

--- client/src/jsx/pages/AdminPanel/Service/index.jsx
+++ client/src/jsx/pages/AdminPanel/Service/index.jsx
@@ -69,7 +69,7 @@ export default class Service extends React.PureComponent {
                     <h3>
                         { _("admin.headers.about") }
                     </h3>
-                    { _("admin.alerts.about", "v1.1", "14.10.2016") }
+                    { _("admin.alerts.about", "v1.1.1", "11.11.2016") }
                 </div>
             </div>
         );
